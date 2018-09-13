11c7cc525631a2b81ce784fa6b2c171c46a7dfa4
Andrey Aires <andrey.aires@gmail.com>
2018-09-13 10:25:47 -0300

    refactor and lint

--- js/rgbExplorer.js
+++ js/rgbExplorer.js
@@ -1,56 +1,92 @@
 //Hexadecimcal Function
+var hexCode = ["#", "00", "00", "00"];
+
 function hex(x) {
-    x = Number(x);
+    "use strict";
+    var hexPair = Number(x);
     var hexDigits = [
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
         "a", "b", "c", "d", "e", "f"
     ];
-    return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+    return Number.isNaN(hexPair)
+    ? "00"
+    : hexDigits[(hexPair - hexPair % 16) / 16] + hexDigits[hexPair % 16];
 }
 
-function renderView(id, val) {
+function renderView() {
+    "use strict";
     //Get Values from inputs
-    let red = document.getElementById("red").value;
-    let green = document.getElementById("green").value;
-    let blue = document.getElementById("blue").value;
+    var red = document.getElementById("red").value;
+    var green = document.getElementById("green").value;
+    var blue = document.getElementById("blue").value;
 
     //Render View
     document.getElementById("render_red").setAttribute(
-        "style", "background-color: " + "rgb(" + red + ", 0, 0)");
+        "style",
+        "background-color: " + "rgb(" + red + ", 0, 0)"
+    );
     document.getElementById("render_green").setAttribute(
-        "style", "background-color: " + "rgb(0, " + green + ", 0)");
+        "style",
+        "background-color: " + "rgb(0, " + green + ", 0)"
+    );
     document.getElementById("render_blue").setAttribute(
-        "style", "background-color: " + "rgb(0, 0, " + blue + ")");
+        "style",
+        "background-color: " + "rgb(0, 0, " + blue + ")"
+    );
 
-    let hexVal = document.getElementsByClassName("hexcode_" + id);
-    hexVal[0].innerText = hex(val);
+    var hexCodeEl = document.getElementById("hexCode");
+
+    hexCode[1] = hex(red);
+    hexCode[2] = hex(green);
+    hexCode[3] = hex(blue);
+
+    hexCodeEl.innerText = hexCode.join("").toUpperCase();
 }
 
 //Sliders Range Event Listener
-let sliders = document.querySelectorAll("input[type=range]");
-for (let i = 0; i < sliders.length; i++) {
-    sliders[i].addEventListener("input", function () {
-        let id = this.id;
-        let val = this.value;
+function updateRangeInputValue() {
+    "use strict";
+    var id = this.id;
+    var val = this.value;
 
-        // Update Number Input with it's respective Sliders Range Input value
-        let inputNumber = document.getElementsByClassName(id);
-        inputNumber[0].value = val;
-        renderView(id, val);
-    }, false);
+    // Update Number Input with it's respective Sliders Range Input value
+    var inputNumber = document.getElementsByClassName(id);
+    inputNumber[0].value = val;
+    renderView();
 }
+function rangeEvents() {
+    "use strict";
+    const SLIDERS = document.querySelectorAll("input[type=range]");
+    var i = 0;
+    while (i < SLIDERS.length) {
+        SLIDERS[i]
+        .addEventListener("input", updateRangeInputValue, false);
+        i += 1;
+    }
+}
+
 
 //Number Input Event Lister
-let numberInput = document.querySelectorAll("input[type=number]");
-for (let i = 0; i < numberInput.length; i++) {
-    numberInput[i].addEventListener("input", function () {
+function updateNumberInputValue() {
+    "use strict";
+    var id = this.getAttribute("class");
+    var val = this.value;
 
-        let id = this.getAttribute('class');
-        let val = this.value;
-
-        // Update Sliders Range Input with it's respective Number Input value
-        let inputSlider = document.getElementById(id);
-        inputSlider.value = val;
-        renderView(id, val);
-    }, false);
+    // Update Sliders Range Input with it's respective Number Input value
+    var inputSlider = document.getElementById(id);
+    inputSlider.value = val;
+    renderView();
 }
+function numberEvents() {
+    "use strict";
+    const NUMBERS = document.querySelectorAll("input[type=number]");
+    var i = 0;
+    while (i < NUMBERS.length) {
+        NUMBERS[i]
+        .addEventListener("input", updateNumberInputValue, false);
+        i += 1;
+    }
+}
+
+rangeEvents();
+numberEvents();
